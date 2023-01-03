e722ac259d43a7bc54cd4d8dc570b8f61df3e788
@charly-13 <carloscc_1997@outlook.com>
2023-01-02 19:54:40 -0600

    primeros modelos, controladores migrations

---
+++ api/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('/api/marcas', 'MarcaController');
+Route::apiResource('/api/medidas', 'MedidaController');
+Route::apiResource('/api/categorias', 'CategoriaController');
+
+Route::get('/', function () {
+    return view('welcome');
+});
